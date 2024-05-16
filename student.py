6db8cbcca391c6f34324c261a54e0525c38b466c
essa0044 <124932316+essa0044@users.noreply.github.com>
2024-05-16 17:11:23 +0000

    Added my student details

--- student.py
+++ student.py
@@ -1,9 +1,11 @@
 def matriculation_number():
-    x = 10
+    matriculation_number = "00147165"
     pass
 
 def first_name():
+    first_name = "Essa"
     pass
 
 def last_name():
+    last_name = "Ahmad"
     pass
